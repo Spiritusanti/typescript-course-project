37ada240c16dd0c44277c5333cbe2753d6cdea34
Spiritusanti <mccrackenjacob@gmail.com>
2021-01-13 10:03:53 -0800

    added more notes on basic types and a new app.ts file to practice them with

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
-function add(n1, n2, showResult) {
-    if (showResult) {
-        return n1 + n2;
-    }
+// const person: {
+//     name: string;
+//     age: number;
+// } = {
+var person = {
+    name: 'Steve',
+    age: 30,
+    hobbies: ['aquscaping', 'powerlifiting']
+};
+var favoriteActivities;
+favoriteActivities = ['sports'];
+console.log(person);
+for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
+    var hobby = _a[_i];
+    console.log(hobby);
 }
-;
-var number1 = 5;
-var number2 = 3.5;
-var printResult = true;
-var result = add(number1, number2, printResult);
-console.log(result);
--- app.ts
+++ app.ts
@@ -1,12 +1,20 @@
-function add(n1: number, n2: number, showResult: boolean) {
-    if(showResult){
-        return n1 + n2
-    }
+const person: {
+    name: string;
+    age: number;
+    hobbies: string[];
+    role: [number, string];
+} = {
+    name: 'Steve',
+    age: 30,
+    hobbies: ['aquscaping', 'powerlifiting'],
+    role: [2, 'author'],
 };
 
-const number1 = 5;
-const number2 = 3.5;
-const printResult = true;
+let favoriteActivities: string[];
+favoriteActivities = ['sports'];
 
-const result = add(number1, number2, printResult);
-console.log(result)
+console.log(person);
+
+for (const hobby of person.hobbies) {
+    console.log(hobby);
+}
